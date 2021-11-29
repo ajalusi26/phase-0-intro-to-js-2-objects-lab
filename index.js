ac2eb835477542f94bb5da7c41270d12fd6a276e
AJ Jalusi <ajalusi26@gmail.com>
2021-11-29 11:06:55 -0500

    Completed Assignments

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution in this file!
+const employee = {
+    name:  "Mark",
+    streetAddress: "3451 Random Address" ,
+};
+
+function updateEmployeeWithKeyAndValue (employee, key, value){
+    employee = {...employee};
+    employee.name = "Sam";
+    employee.streetAddress = value;
+    return employee
+   }
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee.name = "Sam";
+    employee.streetAddress = value;
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    employee = {...employee};
+    delete employee.name;
+    return employee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee.name;
+    return employee
+}
+
+
+
